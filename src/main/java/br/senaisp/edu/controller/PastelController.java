7e0c05d40859170673ce98729643d2879a7eda46
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-14 20:23:23 -0300

    feat: update to delete item

--- src/main/java/br/senaisp/edu/controller/PastelController.java
+++ src/main/java/br/senaisp/edu/controller/PastelController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +40,7 @@ public class PastelController {
 		}
 		
 		@CrossOrigin(origins = "http://localhost:3000")
-		@GetMapping("/delete/{id}")
+		@DeleteMapping("/delete/{id}")
 		public List<Pastel> deletePastel(@PathVariable Integer id) {
 			repository.delete(id);
 			
