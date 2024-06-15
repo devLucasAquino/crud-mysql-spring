383c5a639a596e4e19d10ffb9eab3dfbcdd3a3f6
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-14 21:13:29 -0300

    fix: ajustado para @PutMapping

--- src/main/java/br/senaisp/edu/controller/PastelController.java
+++ src/main/java/br/senaisp/edu/controller/PastelController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,8 +54,9 @@ public class PastelController {
 			
 		}
 		@CrossOrigin(origins = "http://localhost:3000")
-		@GetMapping("/update/{id}/{sabor}")
+		@PutMapping("/update/{id}/{sabor}")
 		public List<Pastel> updatePastel(@PathVariable String sabor, @PathVariable Integer id){
+			 System.out.println("Requisição recebida para atualizar id: " + id + " com sabor: " + sabor);
 			repository.altera(id, sabor);
 			
 			return repository.lista();
